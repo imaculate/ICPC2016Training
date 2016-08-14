91677b980e3c816d5ed9e62b1ddfd037cc63241c
imaculate <imaculatemosha@yahoo.com>
2016-08-14 18:21:59 +0200

    Ensured truth

--- Problems/EnsuringTruth/Main.java
+++ Problems/EnsuringTruth/Main.java
@@ -5,35 +5,51 @@ public class Main{
       int N = Integer.parseInt(k.nextLine());
       for(int i=0; i<N; i++){
          String line = k.nextLine();
-         String[] arrs = line.split("|");
+         //System.out.println(line);
+          boolean comps = false;
+          //System.out.println(line.contains("|"));
+         String[] arrs = line.split("\\|");
+         
+
          for(int j= 0;j< arrs.length; j++){
             String s = arrs[j];
+            //System.out.println(s);
             s = s.replace("(", "").replace(")", "");
-            System.out.println(s);
+            boolean prod = true;
+
+            
             String[] arr2 = s.split("&");
             HashMap<Character, Boolean> map  = new HashMap<Character, Boolean>();
             for(int m = 0; m<arr2.length; m++){
                String st = arr2[m];
+               //System.out.println(st);
                boolean n = false;
-               char c = '';
-               if(st.length>1){
-                  c = st.charAt(i);
+              char c = '0';
+               if(st.length()>1){
+                  c = st.charAt(1);
                   
                }else{
-                  c = st.charAt(i);
+                  c = st.charAt(0);
                   n = true;
                }
-               if(map.contains(c) && map.get(c)!= n)
+               if(map.containsKey(c) && map.get(c)!= n){//found complements
+                 
+                  prod = false;
+                  break;
+               }else{
+                  map.put(c,n);
+               }
             }
-            
+            comps  = comps | prod;
+           
+             
          }
+          if(comps)
+               System.out.println("YES");
+          else
+              System.out.println("NO");
       }
    }
    
-   public static boolean opposites(String a, String b){
-      if(a.length() ==2)
-         return a.charAt(1) == b.charAt(0);
-      else
-        return a.charAt(0) == b.charAt(1);
-   }
+
 }
